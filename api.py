9d365403f3f2715e433548789b3565a9f0557dbb
Michael Hermann-Hubler <39239606+MichaelHermannHubler@users.noreply.github.com>
2022-01-12 20:41:26 +0100

    Testcases added

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import random
 import string
 import base64
+import os
 
 from flask import Flask
 from flask.helpers import send_from_directory
@@ -17,6 +18,9 @@ def get_random_string(length):
 @app.route("/file/<path:base64string>", methods=['POST'])
 def upload(base64string):
     filename = get_random_string(50)
+    if not os.path.isdir(DIRECTORY_LOCATION):
+        os.mkdir(DIRECTORY_LOCATION)
+
     with open(DIRECTORY_LOCATION + filename, "wb") as fh:
         fh.write(base64.b64decode(base64string))
     print('wrote file to', DIRECTORY_LOCATION + filename)
