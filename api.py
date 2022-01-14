4709e99e4214041e51af224f8c8ab260ea9829fa
Michael Hermann-Hubler <39239606+MichaelHermannHubler@users.noreply.github.com>
2022-01-14 17:12:42 +0100

    Quick fix, upload additional parameter

--- api.py
+++ api.py
@@ -15,16 +15,32 @@ def get_random_string(length):
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
     
-@app.route("/file/<path:base64string>", methods=['POST'])
-def upload(base64string):
-    filename = get_random_string(50)
+@app.route("/file/<name>/<path:base64string>", methods=['POST'])
+def upload(name, base64string):
     if not os.path.isdir(DIRECTORY_LOCATION):
         os.mkdir(DIRECTORY_LOCATION)
 
-    with open(DIRECTORY_LOCATION + filename, "wb") as fh:
-        fh.write(base64.b64decode(base64string))
-    print('wrote file to', DIRECTORY_LOCATION + filename)
-    return filename
+    path = DIRECTORY_LOCATION + name + '/'
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    onlyfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    filenname = 'IMG'
+    ending = '.jpg'
+    
+    if ending == '.jpg':
+        i = 1
+        while (filenname + str(i) + ending) in onlyfiles:
+            i = i + 1
+        
+        with open(path + filenname + str(i) + ending, "wb") as fh:
+            fh.write(base64.b64decode(base64string))
+        print('wrote file to', path + filenname + str(i) + ending)
+    else:
+        with open(name + ending, "wb") as fh:
+            fh.write(base64.b64decode(base64string))
+
+    return 'Success'
 
 @app.route("/file/<fileCode>", methods=['GET'])
 def download(fileCode):
