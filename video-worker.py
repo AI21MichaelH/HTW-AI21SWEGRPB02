f659eee2d8e173cd9539e1b27db497436f245e4c
Michael Hermann-Hubler <39239606+MichaelHermannHubler@users.noreply.github.com>
2022-01-19 19:02:43 +0100

    include rabbitMQ handling

--- video-worker.py
+++ video-worker.py
@@ -2,6 +2,10 @@ import base64
 import os
 import shutil
 
+import atexit
+import pika
+from threading import Thread
+
 import cv2
 import requests
 
@@ -12,8 +16,9 @@ downloadUrl = fileRepoUrl + '/file/{name}'
 uploadUrl = fileRepoUrl + '/file/{name}/{base64String}'
 tempdir = 'temp/'
 
-def onCall(videoName):
-    url = downloadUrl.replace('{name}', videoName)
+def onCall(ch, method, properties, body):
+    videoName = body.split('|')[1]
+    url = downloadUrl.format(name = videoName)
     videoPath = tempdir + videoName + '/' + videoName + '.avi'
 
     response = requests.get(url)
@@ -40,8 +45,54 @@ def onCall(videoName):
         encoded_string = encoded_string.decode('ascii')
     shutil.rmtree(tempdir + videoName)
 
-    url = uploadUrl.replace('{name}', videoName).replace('{base64String}', encoded_string)
+    url = uploadUrl.format(name = videoName, base64String = encoded_string)
     response = requests.post(url)
 
+    if not config.TEST_MODE:
+        channelProducer.basic_publish(exchange='',
+                        routing_key='hello',
+                        body='New Video available|{name}'.format(name='videoName'))
+        print("published a new video")    
+
 if config.TEST_MODE:
-    onCall('test')
+    onCall('', '', '', 'Video request|test')
+else:
+    rabbitMqUrl ='amqp://ai21-ws21-swe-rabbitmq?connection_attempts=5&retry_delay=4'
+
+    # Producer
+    print('setting up producer connection to rabbitMq using URL', rabbitMqUrl)
+    connectionProducer = pika.BlockingConnection(pika.URLParameters(rabbitMqUrl))
+    print('established producer connection to rabbitMq')
+    channelProducer = connectionProducer.channel()
+    channelProducer.queue_declare(queue='hello')
+    print('declared producer rabbitmq queue \'hello\'')
+
+    # Consumer
+    print('setting up consumer connection to rabbitMq using URL', rabbitMqUrl)
+    connectionConsumer = pika.BlockingConnection(pika.URLParameters(rabbitMqUrl))
+    print('established consumer connection to rabbitMq')
+    channelConsumer = connectionConsumer.channel()
+    channelConsumer.queue_declare(queue='hello')
+    print('declared consumer rabbitmq queue \'hello\'') 
+
+    print('before basic_consume')
+    channelConsumer.basic_consume(queue='hello',
+                        auto_ack=True,
+                        on_message_callback=onCall)
+    print('after basic_conume; before start_consuming')
+
+    def startConsuming():    
+        print('startConsuming: before start_consuming')
+        channelConsumer.start_consuming()
+        print('startConsuming: after start_consuming')
+
+    thread = Thread(target = startConsuming)
+    thread.start()
+    print('after thread.start()')
+
+    def close_rabbitmq_connection():
+        connectionProducer.close()
+        connectionConsumer.close()
+        print('Closed rabbitmq connections')
+
+    atexit.register(close_rabbitmq_connection)
